c72697f43a2038a7c4d61f34a00009bc9fcce1b0
lvsh <2429894846@qq.com>
2023-08-30 19:13:41 +0800

    Revert "[nvim] More abbreviations"
    This reverts commit a5d5eba7e8eb1fda3feaba66cbb1c040a759aaf8.

--- .config/nvim/lua/plugins/git-integrations.lua
+++ .config/nvim/lua/plugins/git-integrations.lua
@@ -7,8 +7,6 @@ return {
             vim.o.statusline = "%<%f %h%m%r%{FugitiveStatusline()}%=%-14.(%l,%c%V%) %P"
 
             vim.cmd.cnoreabbrev("git Git")
-            vim.cmd.cnoreabbrev("Gblame Git blame")
-            vim.cmd.cnoreabbrev("Gcommit Git commit")
         end,
     },
 }
