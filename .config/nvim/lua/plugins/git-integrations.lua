f175f49b57bc62df2861ea868b10dcef18c89a41
lvsh <2429894846@qq.com>
2023-08-30 16:28:16 +0800

    [nvim] Show git branch in the status line

--- .config/nvim/lua/plugins/git-integrations.lua
+++ .config/nvim/lua/plugins/git-integrations.lua
@@ -4,6 +4,8 @@ return {
         event = "VeryLazy",
         -- cmd = "Git",
         config = function()
+            vim.o.statusline = "%<%f %h%m%r%{FugitiveStatusline()}%=%-14.(%l,%c%V%) %P"
+
             vim.cmd.cnoreabbrev("git Git")
             vim.cmd.cnoreabbrev("Gblame Git blame")
             vim.cmd.cnoreabbrev("Gcommit Git commit")
