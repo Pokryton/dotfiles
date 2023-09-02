8edc1373e6b1e9ffa8302094dbec0b62ed189fa4
lvsh <2429894846@qq.com>
2023-09-02 17:45:38 +0800

    [nvim] Remove "cnoreabbrev git Git"

--- .config/nvim/lua/plugins/git-integrations.lua
+++ .config/nvim/lua/plugins/git-integrations.lua
@@ -2,11 +2,8 @@ return {
     {
         "tpope/vim-fugitive",
         event = "VeryLazy",
-        -- cmd = "Git",
         config = function()
             vim.o.statusline = "%<%f %h%m%r%{FugitiveStatusline()}%=%-14.(%l,%c%V%) %P"
-
-            vim.cmd.cnoreabbrev("git Git")
         end,
     },
 }
