99ce1984c7b9d276cb50afa5d34357cac8f59c34
Lv Sihan <2429894846@qq.com>
2023-09-26 23:36:26 +0800

    [nvim] oil.nvim settings

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -1,9 +1,23 @@
 return {
     "stevearc/oil.nvim",
     lazy = false,
-    keys = { { "-", "<Cmd>Oil<CR>" } },
+    keys = {
+        { "-", "<Cmd>Oil<CR>" }
+    },
     opts = {
-        columns = { "permissions", "size", "mtime" },
-        view_options = { show_hidden = true }
+        columns = {
+            "permissions",
+            "size",
+            "mtime"
+        },
+        skip_confirm_for_simple_edits = true,
+        view_options = {
+            show_hidden = true
+        },
+        keymaps = {
+            ["`"] = "actions.tcd",
+            ["~"] = "actions.cd",
+            ["."] = "actions.open_cmdline"
+        }
     }
 }
