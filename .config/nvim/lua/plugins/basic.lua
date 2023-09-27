14ff9394232eb5c8bb14e74fd812c06064726f7a
Lv Sihan <2429894846@qq.com>
2023-09-27 08:45:38 +0800

    Revert "[nvim] try vim-rhubarb"
    This reverts commit 2f9d0f8a941dbf8e1fbad306e975a25751e74f41.

--- .config/nvim/lua/plugins/basic.lua
+++ .config/nvim/lua/plugins/basic.lua
@@ -12,7 +12,6 @@ return {
     {
         "tpope/vim-fugitive",
         event = "VeryLazy",
-        dependencies = { "tpope/vim-rhubarb" },
         keys = { { "<Leader>g", ":G" } },
         config = function()
             vim.o.statusline = "%<%f %h%m%r%{FugitiveStatusline()}%=%-14.(%l,%c%V%) %P"
