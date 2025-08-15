f4f89c275f92adc2401d311d536008c6e7c0c161
Lv Sihan <2429894846@qq.com>
2025-08-15 17:41:48 +0800

    [nvim] telescope new bindings

--- .config/nvim/lua/plugins/telescope.lua
+++ .config/nvim/lua/plugins/telescope.lua
@@ -9,6 +9,8 @@ return {
   keys = {
     { "<Leader>f", function() require("telescope.builtin").find_files() end },
     { "<Leader>;", function() require("telescope.builtin").buffers() end },
+    { "<Leader>S", function() require("telescope.builtin").live_grep() end },
+    { "<Leader>R", function() require("telescope.builtin").command_history({sorter = require("telescope.sorters").get_substr_matcher()}) end },
   },
   config = function()
     local actions = require("telescope.actions")
